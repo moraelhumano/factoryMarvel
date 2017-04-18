4221f515498e9b748957f7c17f39ee89b1420775
moraelhumano <moraelhumano@gmail.com>
2017-04-18 00:15:41 -0500

    factory marvel

---
+++ app/character_component/character_component.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict'
+
+    var hero = {
+        templateUrl:"app/character_component/hero.html",
+        controller:characterCtrl,
+    }
+
+    function characterCtrl(marvelData) {
+        var char = this;
+
+        char.superheros = null;
+
+        marvelData.get()
+        .$promise
+            .then(function(response){
+                char.superheros = response.data.results;
+                console.log(char.superheros)
+            })
+    }
+
+    angular
+        .module('marvel')
+        .component('hero',hero);
+})();
